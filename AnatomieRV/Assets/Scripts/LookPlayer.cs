aba442a931c65d5bfcaa9af7516b62509aff1e07
Unknown <objecris@gmail.com>
2019-05-23 18:27:36 -0400

    Colliders refaits. Possibilité de reset la position d'un os/muscle

---
+++ AnatomieRV/Assets/Scripts/LookPlayer.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LookPlayer : MonoBehaviour
+{
+    public GameObject VRCamera;
+    public GameObject FallbackCamera;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (VRCamera.activeInHierarchy)
+            transform.LookAt(VRCamera.transform);
+        else
+            transform.LookAt(FallbackCamera.transform);
+        transform.Rotate(0, 180, 0);
+    }
+}
---
+++ AnatomieRV/Assets/Scripts/ResetPiece.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ResetPiece : MonoBehaviour
+{
+    Vector3 posOrigin;
+    Quaternion rotOrigin;
+    Transform parent;
+
+    public SphereCollider resetSphere;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        posOrigin = transform.localPosition;
+        rotOrigin = transform.localRotation;
+        parent = transform.parent;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void CheckResetPos()
+    {
+        if ((transform.position - (resetSphere.center + resetSphere.transform.position)).sqrMagnitude < resetSphere.radius * resetSphere.radius)
+        {
+            ResetPos();
+        }
+    }
+
+    void ResetPos()
+    {
+        transform.SetParent(parent);
+        transform.localPosition = posOrigin;
+        transform.localRotation = rotOrigin;
+    }
+}
